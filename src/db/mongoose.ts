f7a40ef2437b3cc5b73c55c5c0ac2a44e55046a7
Samuel Lorenzo Sánchez <48592338+alu010210681@users.noreply.github.com>
2024-05-01 12:26:58 +0000

    Deploy Update

--- src/db/mongoose.ts
+++ src/db/mongoose.ts
@@ -1,6 +1,6 @@
 import { connect } from 'mongoose';
 
-connect('mongodb://127.0.0.1:27017/ikea-api').then(() => {
+connect('mongodb+srv://dsiikea:dsiikea@ikearestapicluster.0xjelzj.mongodb.net/ikea-api').then(() => {
   console.log('Connection to MongoDB server established');
 }).catch(() => {
   console.log('Unable to connect to MongoDB server');
